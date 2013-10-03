a44bcdd4cc618a402ebd5cce061c828855e03219
royglez <rodrigo.gonzalez.alba@gmail.com>
2013-10-03 17:12:41 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,46 @@
+package application;
+
+import org.opencv.core.Core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+	
+	public static Boolean exit = false;
+	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME); //Load the OpenCV Library
+	}
+	
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("windowApp.fxml"));
+			Scene scene = new Scene(root,600,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setTitle("Image Viewer");
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(Main.class, args);
+	
+		Runtime.getRuntime().addShutdownHook(new Thread()
+	    {
+	        @Override
+	        public void run()
+	        {
+	            exit = true;
+	        }
+	    });
+	}
+	
+}
