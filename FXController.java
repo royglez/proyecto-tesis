0e74f7511420285e2641017da271e7fd63e72514
royglez <rodrigo.gonzalez.alba@gmail.com>
2013-10-14 17:34:23 -0500

    Update FXController.java

--- FXController.java
+++ FXController.java
@@ -5,7 +5,6 @@ import java.awt.image.BufferedImage;
 import facefinder.ImageSource;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
@@ -19,16 +18,13 @@ public class FXController extends Application {
 	SimpleObjectProperty<Image> imageProperty = new SimpleObjectProperty<Image>();
 	
 	@FXML private ImageView imagePanel;
-	ImageSource imageFromCam = new ImageSource();
 	
 	private AnimationTimer cameraImages  = new AnimationTimer() {
 		@Override
 		public void handle(long arg0) {
-			// TODO Auto-generated method stub
-			BufferedImage img = imageFromCam.getImage();
+			BufferedImage img = ImageSource.getInstance().getImage();
 			Image image = SwingFXUtils.toFXImage(img, null); //Convert from BufferedImage to FX Image
 			setImage(image);
-			System.out.println("Hola");
 		}
 	};
 	
@@ -36,31 +32,14 @@ public class FXController extends Application {
 		imageProperty.set(img);
 	}
 	
-	public final Image getImage(){
-		return imageProperty.get();
-	}
-
-	private void displayImageFromCamera(){		
-		this.cameraImages.start();
-	}
-	
 	@FXML protected void handleShowImageBtn(ActionEvent event) {
 		imagePanel.imageProperty().bind(imageProperty);
-		displayImageFromCamera();
+		this.cameraImages.start();
 	}
 
 	@Override
 	public void start(Stage arg0) throws Exception {
-		Runtime.getRuntime().addShutdownHook(new Thread()
-	    {
-	        @Override
-	        public void run()
-	        {
-	        	System.out.println("Saliendo");
-	            cameraImages.stop();
-	            imageFromCam.destroyCamera();
-	        }
-	    });
+		
 	}
 	
 }
