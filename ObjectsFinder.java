8344e7781793b252a23b008b42b4f8475bb99211
royglez <rodrigo.gonzalez.alba@gmail.com>
2013-10-15 17:17:59 -0500

    Update ObjectsFinder.java

--- ObjectsFinder.java
+++ ObjectsFinder.java
@@ -1,12 +1,18 @@
 package facefinder;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.objdetect.CascadeClassifier;
 
 public class ObjectsFinder {
 	
 	private CascadeClassifier cascadeClassificator =  null;
-	private MatOfRect objects = new MatOfRect();
+	private MatOfRect objectsDetected = new MatOfRect();
 
 	public ObjectsFinder(String cascade){
 		if(cascade != null && !cascade.isEmpty()){
@@ -28,7 +34,22 @@ public class ObjectsFinder {
 		}
 	}
 	
-	public void findObject() {
-		
+	public Mat findObject(Mat image) {
+		if(!cascadeClassificator.empty() && cascadeClassificator != null){
+			Size minImgSize = image.size();
+			minImgSize.width /= 20;
+			minImgSize.height /= 20;
+			cascadeClassificator.detectMultiScale(image, objectsDetected, 1.7, 2, 1, minImgSize, image.size());
+			
+			if(objectsDetected.toArray().length > 0){
+				for(Rect rect : objectsDetected.toArray()) {
+					Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
+				}
+			}
+			return image;
+		}else{
+			System.out.println("The cascade file must not be empty. Please Verify.");
+		}
+		return null;
 	}
 }
